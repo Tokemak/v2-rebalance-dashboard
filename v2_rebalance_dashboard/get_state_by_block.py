4a5180f1eceed297550470fd7fb3f1d42cc91607
parkerburchett <parkerburchett@gmail.com>
2024-08-21 14:59:35 -0400

    got nav  and nav per share plots

--- v2_rebalance_dashboard/get_state_by_block.py
+++ v2_rebalance_dashboard/get_state_by_block.py
@@ -3,27 +3,28 @@ import pandas as pd
 from multicall import Multicall, Call
 import numpy as np
 
-MULTICALL2_DEPLOYMENT_BLOCK = 12336033
-
 from os import environ
 from web3 import Web3
 
+import nest_asyncio
+import asyncio
+
+nest_asyncio.apply()
+
 ALCHEMY_URL = environ["ALCHEMY_URL"]
 eth_client = Web3(Web3.HTTPProvider(ALCHEMY_URL))
+MULTICALL2_DEPLOYMENT_BLOCK = 12336033
+
+
+def sync_get_raw_state_by_block_one_block(calls: list[Call], block: int):
+    return asyncio.run(safe_get_raw_state_by_block_one_block(calls, block))
 
 
 async def safe_get_raw_state_by_block_one_block(calls: list[Call], block: int):
-    multicall = Multicall(
-        calls=calls, block_id=block, _w3=eth_client, require_success=False
-    )
+    # nice for testing
+    multicall = Multicall(calls=calls, block_id=block, _w3=eth_client, require_success=False)
     response = await multicall.coroutine()
     return response
-
-
-def _identity_with_bool_success(success, value):
-    if success:
-        return value
-    return None
 
 
 def _build_default_block_and_timestamp_calls():
@@ -31,20 +32,18 @@ def _build_default_block_and_timestamp_calls():
     get_block_call = Call(
         multicall_v3,
         ["getBlockNumber()(uint256)"],
-        [("block", _identity_with_bool_success)],
+        [("block", identity_with_bool_success)],
     )
 
     get_timestamp_call = Call(
         multicall_v3,
         ["getCurrentBlockTimestamp()(uint256)"],
-        [("timestamp", _identity_with_bool_success)],
+        [("timestamp", identity_with_bool_success)],
     )
     return get_block_call, get_timestamp_call
 
 
-def _data_fetch_builder(
-    semaphore: asyncio.Semaphore, responses: list, failed_multicalls: list
-):
+def _data_fetch_builder(semaphore: asyncio.Semaphore, responses: list, failed_multicalls: list):
     async def _fetch_data(multicall: Multicall):
         async with semaphore:
             try:
@@ -57,7 +56,13 @@ def _data_fetch_builder(
     return _fetch_data
 
 
-async def safe_get_raw_state_by_block(
+def sync_safe_get_raw_state_by_block(
+    calls: list[Call], blocks: list[int], semaphore_limits: int = (300, 100, 30, 10, 1)
+) -> pd.DataFrame:
+    return asyncio.run(async_safe_get_raw_state_by_block(calls, blocks, semaphore_limits))
+
+
+async def async_safe_get_raw_state_by_block(
     calls: list[Call], blocks: list[int], semaphore_limits: int = (300, 100, 30, 10, 1)
 ) -> pd.DataFrame:
     """
@@ -82,16 +87,12 @@ async def safe_get_raw_state_by_block(
     failed_multicalls = []
     calls_remaining = [m for m in pending_multicalls]
     for semaphore_limit in semaphore_limits:
-        print(f"{len(calls_remaining)=} {semaphore_limit=}")
+        # print(f"{len(calls_remaining)=} {semaphore_limit=}")
         # make a lot of calls very fast, then slowly back off and remake the calls that failed
         semaphore = asyncio.Semaphore(semaphore_limit)
         failed_multicalls = []
-        _ratelimited_async_data_fetcher = _data_fetch_builder(
-            semaphore, responses, failed_multicalls
-        )
-        await asyncio.gather(
-            *[_ratelimited_async_data_fetcher(m) for m in calls_remaining]
-        )
+        _ratelimited_async_data_fetcher = _data_fetch_builder(semaphore, responses, failed_multicalls)
+        await asyncio.gather(*[_ratelimited_async_data_fetcher(m) for m in calls_remaining])
 
         calls_remaining = [f for f in failed_multicalls]
         if len(calls_remaining) == 0:
@@ -104,3 +105,24 @@ async def safe_get_raw_state_by_block(
     df.sort_index(inplace=True)
     df.index = pd.to_datetime(df.index, unit="s")
     return df
+
+
+def safe_normalize_with_bool_success(success: int, value: int):
+    if success:
+        return int(value) / 1e18
+    return None
+
+
+def identity_with_bool_success(success, value):
+    if success:
+        return value
+    return None
+
+
+def build_blocks_to_use():
+    current_block = eth_client.eth.block_number
+    start_block = 20062439  # TODO, get a better method for this. blocks are not perfectly line dup
+    approx_blocks_per_day = 7100
+    blocks = [b for b in range(start_block, current_block, approx_blocks_per_day)]
+    # good enough for now, fix later use etherscan
+    return blocks
