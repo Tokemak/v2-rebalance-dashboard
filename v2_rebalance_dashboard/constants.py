b2479221c22adeef4dd718e9d47bd2d843473310
parkerburchett <parkerburchett@gmail.com>
2024-08-29 18:43:25 -0400

    wip

--- v2_rebalance_dashboard/constants.py
+++ v2_rebalance_dashboard/constants.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
+import json
+
 ALCHEMY_URL = environ["ALCHEMY_URL"]
 eth_client = Web3(Web3.HTTPProvider(ALCHEMY_URL))
 
@@ -14,9 +16,6 @@ ROOT_PRICE_ORACLE = "0x28B7773089C56Ca506d4051F0Bc66D247c6bdb3a"
 BALANCER_VAULT_ADDRESS = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
 
 autoETH_AUTOPOOL_ETH_ADDRESS = "0x49C4719EaCc746b87703F964F09C22751F397BA0"
-
-
-<<<<<<< HEAD
 balETH_AUTOPOOL_ETH_ADDRESS = "0x72cf6d7C85FfD73F18a83989E7BA8C1c30211b73"
 balETH_AUTOPOOL_ETH_STRATEGY_ADDRESS = "0xB86723da7d02C91b5E421Ed7883C35f732556F13"
 
@@ -29,6 +28,4 @@ with open(ROOT_DIR / "vault_abi.json", "r") as fin:
 
 with open(ROOT_DIR / "strategy_abi.json", "r") as fin:
     AUTOPOOL_ETH_STRATEGY_ABI = json.load(fin)
-=======
 ROOT_DIR = Path(__file__).parent
->>>>>>> 45d3d7c (wip)
