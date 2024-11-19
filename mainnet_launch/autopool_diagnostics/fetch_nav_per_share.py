c7f608bd031edfd67ccc187cfab384e30eca49b1
parkerburchett <parkerburchett@gmail.com>
2024-11-19 17:27:30 -0500

    wip

--- mainnet_launch/autopool_diagnostics/fetch_nav_per_share.py
+++ mainnet_launch/autopool_diagnostics/fetch_nav_per_share.py
@@ -9,6 +9,7 @@ from mainnet_launch.data_fetching.get_state_by_block import (
 from mainnet_launch.constants import CACHE_TIME, ALL_AUTOPOOLS, AutopoolConstants
 
 
+
 def nav_per_share_call(name: str, autopool_vault_address: str) -> Call:
     return Call(
         autopool_vault_address,
@@ -17,15 +18,15 @@ def nav_per_share_call(name: str, autopool_vault_address: str) -> Call:
     )
 
 
-@st.cache_data(ttl=CACHE_TIME)
-def _fetch_all_all_pool_nav_per_share(blocks):
-    calls = [nav_per_share_call(autopool.name, autopool.autopool_eth_addr) for autopool in ALL_AUTOPOOLS]
-    nav_per_share_df = get_raw_state_by_blocks(calls, blocks)
-    return nav_per_share_df
 
 
 def fetch_nav_per_share(blocks: list[int], autopool: AutopoolConstants) -> pd.DataFrame:
-    nav_per_share_df = _fetch_all_all_pool_nav_per_share(blocks)[[autopool.name]]
+
+    nav_per_share_df = get_raw_state_by_blocks(
+        calls=[nav_per_share_call(autopool.name, autopool.autopool_eth_addr)], blocks=blocks, chain=autopool.chain
+    )
+
+    # nav_per_share_df = _fetch_all_all_pool_nav_per_share(blocks)[[autopool.name]]
     nav_per_share_df = nav_per_share_df.resample("1D").last()
 
     # Calculate the 30-day difference and annualized return
