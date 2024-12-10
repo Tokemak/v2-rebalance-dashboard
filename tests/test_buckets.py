d6dd86ecb0d0094b2f2fc1d14b44af8fd42a4944
parkerburchett <parkerburchett@gmail.com>
2024-12-10 09:07:28 -0500

    added print

--- tests/test_buckets.py
+++ tests/test_buckets.py
@@ -8,6 +8,10 @@ def test_s3_bucket_access():
     s3_client = boto3.client("s3", config=Config(signature_version=UNSIGNED))
 
     for autopool in ALL_AUTOPOOLS:
-        bucket_name = autopool.solver_rebalance_plans_bucket
-        response = s3_client.list_objects_v2(Bucket=bucket_name)
-        assert response.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200
+        try:
+            bucket_name = autopool.solver_rebalance_plans_bucket
+            response = s3_client.list_objects_v2(Bucket=bucket_name)
+            assert response.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200
+        except Exception as e:
+            print(autopool.name, 'failed')
+            raise e
