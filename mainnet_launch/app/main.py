bda42f26851911f36875fec58f8529bf780afe47
parkerburchett <parkerburchett@gmail.com>
2025-01-10 13:03:46 -0500

    wip

--- mainnet_launch/app/main.py
+++ mainnet_launch/app/main.py
@@ -1,26 +1,20 @@
-from mainnet_launch.app.ui_config_setup import config_plotly_and_streamlit, STREAMLIT_MARKDOWN_HTML
+from mainnet_launch.app.ui_config_setup import config_plotly_and_streamlit, STREAMLIT_MARKDOWN_HTML, format_timedelta
 
 config_plotly_and_streamlit()
 
 from datetime import datetime
 import streamlit as st
 import logging
-import psutil
 import os
 
 
-from mainnet_launch.constants import ALL_AUTOPOOLS, PRODUCTION_LOG_FILE_NAME, DB_FILE
+from mainnet_launch.constants import ALL_AUTOPOOLS, PRODUCTION_LOG_FILE_NAME, ROOT_DIR
 from mainnet_launch.pages.page_functions import (
     CONTENT_FUNCTIONS,
     PAGES_WITHOUT_AUTOPOOL,
 )
 from mainnet_launch.app.run_on_startup import first_run_of_db
-
-
-def get_memory_usage():
-    process = psutil.Process()
-    mem_info = process.memory_info()
-    return mem_info.rss / (1024**2)
+import portalocker
 
 
 production_logger = logging.getLogger("production_logger")
@@ -34,54 +28,52 @@ if not production_logger.hasHandlers():
     production_logger.propagate = False
 
 
-def format_timedelta(td):
-    """Format a timedelta object into a readable string."""
-    total_seconds = int(td.total_seconds())
-    hours, remainder = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    milliseconds = int(td.microseconds / 1000)
-    formatted_time = ""
-    if hours > 0:
-        formatted_time += f"{hours}h "
-    if minutes > 0 or hours > 0:
-        formatted_time += f"{minutes}m "
-    formatted_time += f"{seconds}s {milliseconds}ms"
-    return formatted_time
+FINISHED_STARTUP_FILE = ROOT_DIR / "app/finished_startup.txt"
+
+
+def render_ui():
+    st.markdown(STREAMLIT_MARKDOWN_HTML, unsafe_allow_html=True)
+    st.title("Autopool Diagnostics Dashboard")
+    st.sidebar.title("Navigation")
+
+    names = [autopool.name for autopool in ALL_AUTOPOOLS]
+    pool_name = st.sidebar.selectbox("Select Pool", names)
+    autopool_name_to_constants = {a.name: a for a in ALL_AUTOPOOLS}
+    autopool = autopool_name_to_constants[pool_name]
+
+    page = st.sidebar.radio("Go to", CONTENT_FUNCTIONS.keys())
+
+    if page in PAGES_WITHOUT_AUTOPOOL:
+        start = datetime.now()
+        CONTENT_FUNCTIONS[page]()
+        time_taken = format_timedelta(datetime.now() - start)
+        production_logger.info(f"Success {page=} {time_taken=}")
+    else:
+        start = datetime.now()
+        CONTENT_FUNCTIONS[page](autopool)
+        time_taken = format_timedelta(datetime.now() - start)
+        production_logger.info(f"Success {page=} {autopool.name=} {time_taken=}")
 
 
 def main():
+    if not os.path.exists(FINISHED_STARTUP_FILE):
+        st.title("Startup Process")
+        st.warning("Keep this tab open and don't refresh after clicking start. This may take ~15 minutes.")
 
-    if not os.path.exists(DB_FILE):
-        
-        if st.button("do first run"):
-            first_run_of_db()
+        if st.button("Start Startup Process"):
+            first_run_of_db(production_logger)
 
-   
-        st.markdown(STREAMLIT_MARKDOWN_HTML, unsafe_allow_html=True)
-        st.title("Autopool Diagnostics Dashboard")
-        st.sidebar.title("Navigation")
+            with open(FINISHED_STARTUP_FILE, "x") as _:
+                pass
 
-        names = [autopool.name for autopool in ALL_AUTOPOOLS]
-        pool_name = st.sidebar.selectbox("Select Pool", names)
-        autopool_name_to_constants = {a.name: a for a in ALL_AUTOPOOLS}
-        autopool = autopool_name_to_constants[pool_name]
-
-        page = st.sidebar.radio("Go to", CONTENT_FUNCTIONS.keys())
-
-        if page in PAGES_WITHOUT_AUTOPOOL:
-            start = datetime.now()
-            production_logger.info(f"Attempting {page}")
-            CONTENT_FUNCTIONS[page]()
-            time_taken = format_timedelta(datetime.now() - start)
-            production_logger.info(f"Success {page=} {time_taken=}")
-        else:
-            start = datetime.now()
-            production_logger.info(f"Attempting {page} {autopool.name}")
-            CONTENT_FUNCTIONS[page](autopool)
-            time_taken = format_timedelta(datetime.now() - start)
-            production_logger.info(f"Success {page=} {autopool.name=} {time_taken=}")
+        try:
+            with open(PRODUCTION_LOG_FILE_NAME, "r") as log_file:
+                log_contents = log_file.read()
+            st.text_area("Production Log", log_contents, height=300)
+        except FileNotFoundError:
+            st.text("Log file not found yet.")
     else:
-        st.text("Populating database for the first time...")
+        render_ui()
 
 
 if __name__ == "__main__":
