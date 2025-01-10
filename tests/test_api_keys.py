8489303f328f00e512e250e03a5c125931016530
parkerburchett <parkerburchett@gmail.com>
2025-01-10 14:44:52 -0500

    wip

--- tests/test_api_keys.py
+++ tests/test_api_keys.py
@@ -1,7 +1,15 @@
+"""Makes sure the API keys are correct"""
+
 import boto3
 from botocore.config import Config
 from mainnet_launch.constants import ALL_AUTOPOOLS
 from botocore import UNSIGNED
+from mainnet_launch.constants import eth_client, base_client
+
+
+def test_block_timestamp():
+    assert eth_client.eth.get_block(20_000_000).timestamp == 1717281407
+    assert base_client.eth.get_block(20_000_000).timestamp == 1726789347
 
 
 def test_s3_bucket_access():
