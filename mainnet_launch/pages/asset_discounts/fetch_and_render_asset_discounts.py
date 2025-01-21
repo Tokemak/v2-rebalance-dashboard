4ddabc0a87e467fc5ba853a6115d708615e5be4e
parkerburchett <parkerburchett@gmail.com>
2025-01-21 18:31:31 -0500

    wip

---
+++ mainnet_launch/pages/asset_discounts/fetch_and_render_asset_discounts.py
@@ -0,0 +1,14 @@
+# charts
+
+# top level
+
+# line plot of % discount of each LST asset
+
+# Overlay events and the spot discount
+
+
+ASSET_DISCOUNT_TABLE = "ASSET_DISCOUNT_TABLE"
+
+# block, timestamp, assset name, root price oracle price, calculator price, in 1.
+
+# get all the calculators
