a02ba8aae02b5c6f8f7532d38a6e41b7b3985250
ParkerBurchett <parkerburchett@gmail.com>
2025-02-19 09:34:51 -0800

    wip

--- mainnet_launch/pages/expected_vs_actual_returns/render_by_destination_realized_apr.py
+++ mainnet_launch/pages/expected_vs_actual_returns/render_by_destination_realized_apr.py
@@ -58,24 +58,6 @@ def _compute_realized_incentive_apr(long_df: pd.DataFrame, n_day_window: int, fo
     return realized_incentive_apr
 
 
-def _compute_n_day_realized_base_and_fee_apr(long_df: pd.DataFrame, base_and_fee_n_day_window: int) -> pd.DataFrame:
-    # the annualized change in virtual price == base apr and fee apr
-    current_virtual_price = (
-        long_df.reset_index().pivot(columns="vault_name", values="virtual_price", index="timestamp").replace(0, np.nan)
-    )
-    virtual_price_after_n_days = current_virtual_price.shift(-base_and_fee_n_day_window)
-
-    annualized_change_in_virtual_price = (365 / base_and_fee_n_day_window) * (
-        (virtual_price_after_n_days - current_virtual_price) / current_virtual_price
-    )
-    annualized_change_in_virtual_price = annualized_change_in_virtual_price.reset_index()
-    annualized_change_in_virtual_price = annualized_change_in_virtual_price.melt(
-        id_vars="timestamp", var_name="vault_name", value_name="realized_base_and_fee_apr"
-    ).set_index(["timestamp", "vault_name"])
-
-    return annualized_change_in_virtual_price
-
-
 def combine_raw_data_into_projected_and_realized_apr_df(
     long_df: pd.DataFrame,
     base_and_fee_n_day_window: int,
@@ -94,13 +76,13 @@ def combine_raw_data_into_projected_and_realized_apr_df(
         long_df["realized_base_and_fee_apr"] + long_df["realized_incentive_apr"]
     )
 
+    long_df["incentive_apr_out_weight"] = incentive_apr_weight * long_df["incentiveAprOut"]
+    long_df["incentive_apr_in_weight"] = incentive_apr_weight * long_df["incentiveAprIn"]
+
     long_df["projected_base_and_fee"] = long_df["baseApr"] + long_df["feeApr"]
-    long_df["projected_apr_out"] = (
-        long_df["baseApr"] + long_df["feeApr"] + (incentive_apr_weight * long_df["incentiveAprOut"])
-    )
-    long_df["projected_apr_in"] = (
-        long_df["baseApr"] + long_df["feeApr"] + (incentive_apr_weight * long_df["incentiveAprIn"])
-    )
+
+    long_df["projected_apr_out"] = long_df["baseApr"] + long_df["feeApr"] + long_df["incentive_apr_out_weight"]
+    long_df["projected_apr_in"] = long_df["baseApr"] + long_df["feeApr"] + long_df["incentive_apr_in_weight"]
 
     return long_df.reset_index()
 
@@ -110,50 +92,37 @@ def combine_raw_data_into_projected_and_realized_apr_df(
 
 def _make_projected_vs_actual_scatter_plot(long_df: pd.DataFrame, x_col, y_col, title):
     fig = px.scatter(long_df, x=x_col, y=y_col, color="vault_name", title=title)
-
     max_value = long_df[[x_col, y_col]].max().max() * 1.2
-
     fig.add_shape(type="line", x0=0, y0=0, x1=max_value, y1=max_value, line=dict(color="black", dash="dash"))
-
-    fig.update_xaxes(range=[0, max_value], scaleanchor="y", scaleratio=1)
-    fig.update_yaxes(range=[0, max_value])
-
     return fig
 
 
-def _render_plots(long_df: pd.DataFrame, incentive_apr_weight: float):
-    long_df_copy = long_df.dropna()
-    long_df_copy["Realized Incentive APR - Projected Incentive APR Out"] = long_df_copy["realized_incentive_apr"] - (
-        long_df_copy["incentiveAprOut"] * incentive_apr_weight
-    )
-    long_df_copy["Realized Incentive APR - Projected Incentive APR In"] = long_df_copy["realized_incentive_apr"] - (
-        long_df_copy["incentiveAprIn"] * incentive_apr_weight
-    )
-    long_df_copy["Realized (Fee + Base APR) - Projected (Base + Fee APR)"] = (
-        long_df_copy["realized_base_and_fee_apr"] - long_df_copy["projected_base_and_fee"]
+def _render_one_kind_of_error(
+    long_df: pd.DataFrame,
+    col: str,
+    predicted_col: str,
+    actual_col: str,
+    error_col: str,
+) -> None:
+    local_long_df =   long_df.sort_values(by="vault_name").reindex(
+        columns=["vault_name", predicted_col, actual_col, error_col]
     )
 
-    long_df_copy["Realized (Fee + Base + Incentive APR) - Projected (Base + Fee + Incentive APR In)"] = (
-        long_df_copy["realized_base_plus_fee_plus_incentive_apr"] - long_df_copy["projected_apr_in"]
-    )
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-
+    with col:
         st.plotly_chart(
             _make_projected_vs_actual_scatter_plot(
-                long_df_copy,
-                "incentiveAprOut",
-                "realized_incentive_apr",
-                "Projected Incentive APR Out vs Realized Incentive APR",
+                local_long_df,
+                predicted_col,
+                actual_col,
+                f"{predicted_col} vs {actual_col}",
             ),
             use_container_width=True,
         )
         st.plotly_chart(
             px.ecdf(
-                long_df_copy,
-                x="Realized Incentive APR - Projected Incentive APR Out",
-                title="Realized Incentive APR - Projected Incentive APR Out",
+                local_long_df,
+                x=error_col,
+                title=error_col,
                 color="vault_name",
             ),
             use_container_width=True,
@@ -161,106 +130,112 @@ def _render_plots(long_df: pd.DataFrame, incentive_apr_weight: float):
 
         st.plotly_chart(
             px.histogram(
-                long_df_copy,
-                x="Realized Incentive APR - Projected Incentive APR Out",
-                title="Realized Incentive APR - Projected Incentive APR Out",
+                local_long_df,
+                x=error_col,
+                title=error_col,
                 color="vault_name",
             ),
             use_container_width=True,
         )
 
-    with col2:
-        st.plotly_chart(
-            _make_projected_vs_actual_scatter_plot(
-                long_df_copy,
-                "projected_base_and_fee",
-                "realized_base_and_fee_apr",
-                "Projected Base + Fee APR In vs Realized Base and Fee",
-            ),
-            use_container_width=True,
-        )
-
-        st.plotly_chart(
-            px.ecdf(
-                long_df_copy,
-                x="Realized (Fee + Base APR) - Projected (Base + Fee APR)",
-                title="Realized (Fee + Base APR) - Projected (Base + Fee APR)",
-                color="vault_name",
-            ),
-            use_container_width=True,
-        )
-
-        st.plotly_chart(
-            px.histogram(
-                long_df_copy,
-                x="Realized (Fee + Base APR) - Projected (Base + Fee APR)",
-                title="Realized (Fee + Base APR) - Projected (Base + Fee APR)",
-                color="vault_name",
-            ),
-            use_container_width=True,
-        )
-
-    with col3:
-        st.plotly_chart(
-            _make_projected_vs_actual_scatter_plot(
-                long_df_copy,
-                "projected_apr_in",
-                "realized_base_plus_fee_plus_incentive_apr",
-                "Projected Base + Fee + Incentive APR Out vs Realized Base + Fee + Incentive",
-            ),
-            use_container_width=True,
-        )
-
-        st.plotly_chart(
-            px.ecdf(
-                long_df_copy,
-                x="Realized (Fee + Base + Incentive APR) - Projected (Base + Fee + Incentive APR In)",
-                title="Realized (Fee + Base + Incentive APR) - Projected (Base + Fee + Incentive APR In)",
-                color="vault_name",
-            ),
-            use_container_width=True,
-        )
-
-        st.plotly_chart(
-            px.histogram(
-                long_df_copy,
-                x="Realized (Fee + Base + Incentive APR) - Projected (Base + Fee + Incentive APR In)",
-                title="Realized (Fee + Base APR) - Projected (Base + Fee APR)",
-                color="vault_name",
-            ),
-            use_container_width=True,
-        )
+        negative_error_stats, positive_error_stats, all_error_stats = _render_error_metrics(local_long_df, error_col)
+        st.subheader("Error Metrics")
+        for stats, label in zip(
+            [negative_error_stats, positive_error_stats, all_error_stats],
+            ["Overestimate (Realized < Projected)", "Underestimate (Realized > Projected)", "All"],
+        ):
+            st.markdown(f"**{label}**")
+            st.write(stats.round(2))
 
 
-def _display_error_stats(long_df: pd.DataFrame):
-
-    long_df_copy = long_df.dropna().copy()
-    long_df_copy["Realized (incentive + base + fee) - Projected (incentive in + base + fee)"] = (
-        long_df_copy["realized_base_plus_fee_plus_incentive_apr"] - long_df_copy["projected_apr_in"]
-    )
+def _render_error_metrics(long_df: pd.DataFrame, error_col: str):
+    long_df_copy = long_df.copy()
     df_copy = long_df_copy.copy()
     df_copy["vault_name"] = "all_destinations"
     long_df_copy = pd.concat([long_df_copy, df_copy], ignore_index=True)
+    names = sorted(long_df["vault_name"].unique().tolist())
+    index_order = ["all_destinations", *names]
 
-    apr_diff_col = "Realized (incentive + base + fee) - Projected (incentive in + base + fee)"
-    error_metrics = ["count", "mean", "median", "max", "min"]
+    error_metrics = [
+        "count",
+        "mean",
+        "median",
+        "max",
+        "min",
+        lambda x: np.percentile(x, 10),
+        lambda x: np.percentile(x, 90),
+    ]
+    error_metric_names = ["count", "mean", "median", "max", "min", "10th percentile", "90th percentile"]
 
-    # --- By Destination Error Stats ---
-    def _make_by_destination_error_stats(df: pd.DataFrame):
-        negative_error_stats = df[df[apr_diff_col] < 0].groupby("vault_name")[apr_diff_col].agg(error_metrics)
-        positive_error_stats = df[df[apr_diff_col] > 0].groupby("vault_name")[apr_diff_col].agg(error_metrics)
-        all_error_stats = df.groupby("vault_name")[apr_diff_col].agg(error_metrics)
-        return negative_error_stats, positive_error_stats, all_error_stats
+    negative_error_stats = long_df_copy[long_df_copy[error_col] < 0].groupby("vault_name")[error_col].agg(error_metrics)
+    negative_error_stats.columns = error_metric_names
+    negative_error_stats = negative_error_stats.loc[index_order]
 
-    neg_stats_dest, pos_stats_dest, all_stats_dest = _make_by_destination_error_stats(long_df_copy)
+    positive_error_stats = long_df_copy[long_df_copy[error_col] > 0].groupby("vault_name")[error_col].agg(error_metrics)
+    positive_error_stats.columns = error_metric_names
+    positive_error_stats = positive_error_stats.loc[index_order]
 
-    st.subheader("Error Metrics")
-    for stats, label in zip(
-        [neg_stats_dest, pos_stats_dest, all_stats_dest],
-        ["Negative (Realized < Projected)", "Positive (Realized > Projected)", "All"],
-    ):
-        st.markdown(f"**{label}**")
-        st.write(stats.round(2))
+    all_error_stats = long_df_copy.groupby("vault_name")[error_col].agg(error_metrics)
+    all_error_stats.columns = error_metric_names
+    all_error_stats = all_error_stats.loc[index_order]
+
+    return negative_error_stats, positive_error_stats, all_error_stats
+
+
+def _render_plots(raw_long_df: pd.DataFrame, incentive_apr_weight: float):
+
+    long_df = raw_long_df.dropna()
+
+    long_df["Realized Incentive APR - Projected Incentive APR Out"] = long_df["realized_incentive_apr"] - (
+        long_df["incentiveAprOut"] * incentive_apr_weight
+    )
+    long_df["Realized (Fee + Base APR) - Projected (Base + Fee APR)"] = (
+        long_df["realized_base_and_fee_apr"] - long_df["projected_base_and_fee"]
+    )
+
+    long_df["Realized (Fee + Base + Incentive APR) - Projected (Base + Fee + Incentive APR Out)"] = (
+        long_df["realized_base_plus_fee_plus_incentive_apr"] - long_df["projected_apr_out"]
+    )
+    col1, col2, col3 = st.columns(3)
+
+    _render_one_kind_of_error(
+        long_df,
+        col1,
+        "incentive_apr_out_weight",
+        "realized_incentive_apr",
+        "Realized Incentive APR - Projected Incentive APR Out",
+    )
+    _render_one_kind_of_error(
+        long_df,
+        col2,
+        "projected_base_and_fee",
+        "realized_base_and_fee_apr",
+        "Realized (Fee + Base APR) - Projected (Base + Fee APR)",
+    )
+
+    _render_one_kind_of_error(
+        long_df,
+        col3,
+        "projected_apr_out",
+        "realized_base_plus_fee_plus_incentive_apr",
+        "Realized (Fee + Base + Incentive APR) - Projected (Base + Fee + Incentive APR Out)",
+    )
+
+
+def _render_one_destination_line_plots(long_df: pd.DataFrame):
+    # Let the user pick one vault_name from a dropdown
+    dest = st.selectbox("Choose a vault", sorted(long_df["vault_name"].unique()))
+
+    pairs = [
+        ["realized_base_and_fee_apr", "projected_base_and_fee"],
+        ["realized_incentive_apr", "incentive_apr_in_weight", "incentive_apr_out_weight"],
+        ["realized_base_plus_fee_plus_incentive_apr", "projected_apr_in", "projected_apr_out"],
+    ]
+
+    sub_df = long_df[long_df["vault_name"] == dest]
+
+    for cols in pairs:
+        st.plotly_chart(px.line(sub_df, x="timestamp", y=cols))
 
 
 def fetch_and_render_by_destination_expected_apr(autopool: AutopoolConstants):
@@ -304,7 +279,7 @@ def fetch_and_render_by_destination_expected_apr(autopool: AutopoolConstants):
     long_df = long_df[long_df["vault_name"].isin(vaults_with_some_tvl)]
 
     _render_plots(long_df, incentive_apr_weight)
-    _display_error_stats(long_df)
+    _render_one_destination_line_plots(long_df)
 
 
 if __name__ == "__main__":
