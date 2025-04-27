4f7ecd64f3f7b591ac7c074add7f7363861e2c3a
parkerburchett <parkerburchett@gmail.com>
2025-04-27 10:37:37 -0700

    wip

--- mainnet_launch/database/schema/ensure_tables_are_current/update_token_values_table.py
+++ mainnet_launch/database/schema/ensure_tables_are_current/update_token_values_table.py
@@ -32,7 +32,6 @@ from mainnet_launch.constants import ALL_CHAINS, ROOT_PRICE_ORACLE, ChainData, S
 def ensure_token_values_are_current():
     for chain in ALL_CHAINS:
         possible_blocks = build_blocks_to_use(chain)
-
         missing_blocks = get_subset_not_already_in_column(
             TokenValues,
             TokenValues.block,
@@ -51,10 +50,10 @@ def ensure_token_values_are_current():
 
         def _extract_token_values_by_row(row: dict):
             for token in all_tokens_orm:
-                backing = row.get(f"{token.token_address}_backing")
+                backing = row.get((token.token_address, "backing"))
                 backing = None if pd.isna(backing) else float(backing)
 
-                safe_price = row[f"{token.token_address}_safe"]
+                safe_price = row[(token.token_address, "safe_price")]
                 safe_price = None if pd.isna(safe_price) else float(safe_price)
 
                 if (safe_price is not None) and (backing is not None):
@@ -82,25 +81,23 @@ def ensure_token_values_are_current():
         )
 
 
-def _build_safe_price_calls(tokens: list[Tokens], chain: ChainData) -> pd.DataFrame:
-    safe_price_eth_calls = [
+def _build_safe_price_calls(tokens: list[Tokens], chain: ChainData) -> list[Call]:
+    return [
         Call(
             ROOT_PRICE_ORACLE(chain),
             ["getPriceInEth(address)(uint256)", t.token_address],
-            [(t.token_address + "_safe", safe_normalize_with_bool_success)],
+            [((t.token_address, "safe_price"), safe_normalize_with_bool_success)],
         )
         for t in tokens
     ]
 
-    # TODO add USDC safe price here later
+    # TODO add USDC safe prichere later
     # get Price in quote, (USDC) see stable coin branch
-
-    return safe_price_eth_calls
 
 
 def _build_backing_calls(tokens: list[Tokens], chain: ChainData) -> list[Call]:
     # this is a self contained problem to make this more readable,
-    # my inclanation is to hard code it there are < 10 LSTs and stablecoins
+    # my inclination is to hard code it there are < 10 LSTs and stablecoins
 
     stats_calculator_registry_contract = chain.client.eth.contract(
         STATS_CALCULATOR_REGISTRY(chain), abi=STATS_CALCULATOR_REGISTRY_ABI
@@ -127,17 +124,18 @@ def _build_backing_calls(tokens: list[Tokens], chain: ChainData) -> list[Call]:
         Call(
             calculatorAddress,
             ["calculateEthPerToken()(uint256)"],
-            [(f"{token_address}_backing", safe_normalize_with_bool_success)],
+            [((Web3.toChecksumAddress(token_address), "backing"), safe_normalize_with_bool_success)],
         )
         for (calculatorAddress, token_address) in zip(lst_calcs["calculatorAddress"], lst_calcs["lst"])
     ]
 
-    dummy_weth_backing_call = make_dummy_1_call(WETH(chain) + "_backing")
+    dummy_weth_backing_call = make_dummy_1_call((WETH(chain), "backing"))
     # TODO add ETH her something like: eeeeeeeeeeeeeeeEEEEEEEeeee version (approx)
     backing_calls.append(dummy_weth_backing_call)
     return backing_calls
 
 
+# move to get raw state by block
 def _constant_1(success, value) -> float:
     return 1.0
 
@@ -154,9 +152,9 @@ def make_dummy_1_call(name: str) -> Call:
 # make sure the backing values are right
 
 
-def _build_lst_backing_dummy_calls():
+def _build_lst_backing_dummy_calls(chain: ChainData):
     # hard coded, these are the LSTs that have a backing of 1 by defenition
-    dummy_weth_backing_call = make_dummy_1_call(WETH(chain) + "_backing")
+    dummy_weth_backing_call = make_dummy_1_call((WETH(chain), "backing"))
 
 
 #     # the backin gis not correct
@@ -170,3 +168,7 @@ def _fetch_safe_and_backing_values(missing_blocks: list[int], tokens: list[Token
     calls = [*_build_safe_price_calls(tokens, chain), *_build_backing_calls(tokens, chain)]
     df = get_raw_state_by_blocks(calls, missing_blocks, chain, include_block_number=True)
     return df
+
+
+if __name__ == "__main__":
+    ensure_token_values_are_current()
