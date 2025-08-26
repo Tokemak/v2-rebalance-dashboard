5713774aa4be64112da12ad18754aa149156d865
parkerburchett <parkerburchett@gmail.com>
2025-08-26 09:15:17 -0700

    wip

--- mainnet_launch/constants/__init__.py
+++ mainnet_launch/constants/__init__.py
@@ -4,3 +4,4 @@ from .chains import *
 from .helpers import *
 from .models import *
 from .secrets import *
+from .chain_base_asset_options import *
--- mainnet_launch/constants/onboarding_new_autopools.md
+++
@@ -1,4 +0,0 @@
-# Steps for adding a new autopool
-
-switch to dev local branch, copy and over write dev local with main,
-
