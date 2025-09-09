803d562da102d724015a46186dedbcd50c01df41
parkerburchett <parkerburchett@gmail.com>
2025-09-09 16:08:19 -0700

    wip

--- mainnet_launch/constants/secrets.py
+++ mainnet_launch/constants/secrets.py
@@ -2,36 +2,37 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+from os import environ
 
 load_dotenv()
 
 
-ALCHEMY_URL = os.getenv("ALCHEMY_URL")
-ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
+ALCHEMY_URL = environ["ALCHEMY_URL"]
+ETHERSCAN_API_KEY = environ["ETHERSCAN_API_KEY"]
 ETHERSCAN_API_URL = "https://api.etherscan.io/v2/api"
 
-COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY")
-DEFAULT_GAS_LIMIT = int(os.getenv("GAS_LIMIT", "550000000"))
+COINGECKO_API_KEY = environ["COINGECKO_API_KEY"]
+DEFAULT_GAS_LIMIT = 550000000
 
 TOKEMAK_SUBGRAPH_URLS = {
-    "eth": os.environ.get("TOKEMAK_ETHEREUM_SUBGRAPH_URL"),
-    "base": os.environ.get("TOKEMAK_BASE_SUBGRAPH_URL"),
-    "sonic": os.environ.get("TOKEMAK_SONIC_SUBGRAPH_URL"),
+    "eth": environ["TOKEMAK_ETHEREUM_SUBGRAPH_URL"],
+    "base": environ["TOKEMAK_BASE_SUBGRAPH_URL"],
+    "sonic": environ["TOKEMAK_SONIC_SUBGRAPH_URL"],
 }
 
 BUCKETS = {
-    "AUTO_ETH": os.environ.get("AUTO_ETH_BUCKET"),
-    "BAL_ETH": os.environ.get("BAL_ETH_BUCKET"),
-    "AUTO_LRT": os.environ.get("AUTO_LRT_BUCKET"),
-    "BASE_ETH": os.environ.get("BASE_ETH_BUCKET"),
-    "DINERO_ETH": os.environ.get("DINERO_ETH_BUCKET"),
-    "AUTO_USD": os.environ.get("AUTO_USD_BUCKET"),
-    "BASE_USD": os.environ.get("BASE_USD_BUCKET"),
-    "AUTO_DOLA": os.environ.get("AUTO_DOLA_BUCKET"),
-    "SONIC_USD": os.environ.get("SONIC_USD_BUCKET"),
-    "BASE_EUR": os.environ.get("BASE_EUR_BUCKET"),
-    "SILO_USD": os.environ.get("SILO_USD_BUCKET"),
-    "SILO_ETH": os.environ.get("SILO_ETH_BUCKET"),
+    "AUTO_ETH": environ["AUTO_ETH_BUCKET"],
+    "BAL_ETH": environ["BAL_ETH_BUCKET"],
+    "AUTO_LRT": environ["AUTO_LRT_BUCKET"],
+    "BASE_ETH": environ["BASE_ETH_BUCKET"],
+    "DINERO_ETH": environ["DINERO_ETH_BUCKET"],
+    "AUTO_USD": environ["AUTO_USD_BUCKET"],
+    "BASE_USD": environ["BASE_USD_BUCKET"],
+    "AUTO_DOLA": environ["AUTO_DOLA_BUCKET"],
+    "SONIC_USD": environ["SONIC_USD_BUCKET"],
+    "BASE_EUR": environ["BASE_EUR_BUCKET"],
+    "SILO_USD": environ["SILO_USD_BUCKET"],
+    "SILO_ETH": environ["SILO_ETH_BUCKET"],
 }
 
 
