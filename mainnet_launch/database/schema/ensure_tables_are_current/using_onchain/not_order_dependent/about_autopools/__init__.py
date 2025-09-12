e1ee6a42b2d31dd488421042e23568480b97b175
parkerburchett <parkerburchett@gmail.com>
2025-09-12 14:14:07 -0700

    wip

--- mainnet_launch/database/schema/ensure_tables_are_current/using_onchain/not_order_dependent/about_autopools/__init__.py
+++ mainnet_launch/database/schema/ensure_tables_are_current/using_onchain/not_order_dependent/about_autopools/__init__.py
@@ -2,8 +2,8 @@ from .update_autopool_fees import ensure_autopool_fees_are_current
 from .update_autopool_vault_deposits import ensure_autopool_deposits_are_current
 from .update_autopool_vault_token_transfers import ensure_autopool_transfers_are_current
 from .update_autopool_vault_withdraws import ensure_autopool_withdraws_are_current
-from .update_autopool_state_base_on_autopool_withdraw_rows import (
-    ensure_an_autopool_state_exists_for_each_autopool_withdrawal,
+from .update_autopool_state_base_on_autopool_deposit_or_withdraw_rows import (
+    ensure_an_autopool_state_exists_for_each_autopool_withdrawal_or_deposit,
 )
 
 
@@ -12,5 +12,5 @@ __all__ = [
     "ensure_autopool_deposits_are_current",
     "ensure_autopool_transfers_are_current",
     "ensure_autopool_withdraws_are_current",
-    "ensure_an_autopool_state_exists_for_each_autopool_withdrawal",
+    "ensure_an_autopool_state_exists_for_each_autopool_withdrawal_or_deposit",
 ]
