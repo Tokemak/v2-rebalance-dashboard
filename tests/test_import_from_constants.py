3f71fcc70d2e755d2c5b548d35d95efec85e34f0
parkerburchett <parkerburchett@gmail.com>
2025-09-15 09:31:30 -0700

    wip

--- tests/test_import_from_constants.py
+++ tests/test_import_from_constants.py
@@ -1,7 +1,4 @@
 import importlib
 
-from mainnet_launch.pages.risk_metrics.drop_down import render_pick_chain_and_base_asset_dropdown
-
-
 def test_import_constants():
     importlib.import_module("mainnet_launch.constants")
