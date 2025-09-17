01a50e307b9245e3fb02b4aa10de20be2e5dbb5e
parkerburchett <parkerburchett@gmail.com>
2025-09-17 10:13:13 -0700

    update ymls

--- mainnet_launch/database/schema/full.py
+++ mainnet_launch/database/schema/full.py
@@ -17,7 +17,16 @@ import uuid
 
 load_dotenv()
 
-tmpPostgres = urlparse(os.getenv("MAIN_DATABASE_URL"))
+which_database = os.getenv("WHICH_DATABASE")
+if which_database is None:
+    raise ValueError("WHICH_DATABASE environment variable not set")
+elif which_database == "MAIN_DATABASE_URL":
+    tmpPostgres = urlparse(os.getenv("MAIN_DATABASE_URL"))
+elif which_database == "MAIN_READ_REPLICA_DATABASE_URL":
+    tmpPostgres = urlparse(os.getenv("MAIN_READ_REPLICA_DATABASE_URL"))
+else:
+    raise ValueError(f"WHICH_DATABASE environment variable set to invalid value: {which_database}")
+
 
 ENGINE = create_engine(
     f"postgresql+psycopg2://{tmpPostgres.username}:{tmpPostgres.password}"
