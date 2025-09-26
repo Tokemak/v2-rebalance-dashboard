1ffb7aa228af76129956aee87fc426351b37d705
parkerburchett <parkerburchett@gmail.com>
2025-09-26 15:39:23 -0700

    wip

--- mainnet_launch/database/schema/ensure_tables_are_current/using_3rd_party/save_local_swap_matrix_all_autopools.py
+++ mainnet_launch/database/schema/ensure_tables_are_current/using_3rd_party/save_local_swap_matrix_all_autopools.py
@@ -36,11 +36,26 @@ def get_autopool_possible_assets(autopool: AutopoolConstants):
     df = _exec_sql_and_cache(query)
 
     if autopool == AUTO_ETH:
-        other_df = _exec_sql_and_cache(AUTO_LRT)
-        df = pd.concat([df, other_df]).drop_duplicates().reset_index(drop=True)
-
-    return df
+        other_df = _exec_sql_and_cache(
+            f"""
     
+        with valid_destinations as (
+    select destination_vault_address from autopool_destinations
+
+    WHERE autopool_destinations.autopool_vault_address = '{AUTO_LRT .autopool_eth_addr}'
+    ),
+
+    this_autopool_asset_tokens as (
+
+    select distinct token_address from destination_tokens 
+
+    WHERE destination_tokens.destination_vault_address in (select destination_vault_address from valid_destinations)
+    )
+
+    select chain_id, token_address, symbol, name, decimals from tokens where tokens.token_address in (select token_address from this_autopool_asset_tokens)"""
+        )
+        df = pd.concat([df, other_df]).drop_duplicates().reset_index(drop=True)
+    return df
 
 
 def fetch_and_save_autopool_swap_matrix_quotes(autopool: AutopoolConstants):
@@ -102,19 +117,19 @@ def main():
     # not sure why arb USD fails, it shouldn't
     # the rest are redundent, there is only one extra asset that justifices inlucidng autoLRT
     # can push into autoETH if needed
-    bad_autopools = [BASE_EUR, SILO_ETH, SONIC_USD, BAL_ETH, DINERO_ETH, ARB_USD, SILO_USD, AUTO_LRT]
+    bad_autopools = [BASE_EUR, SILO_ETH, SONIC_USD, BAL_ETH, DINERO_ETH, ARB_USD, SILO_USD, AUTO_LRT, ARB_USD]
 
     while True:
         for autopool in ALL_AUTOPOOLS:
             if autopool not in bad_autopools:
+
                 try:
                     print(f"Fetching quotes for {autopool.name}")
                     fetch_and_save_autopool_swap_matrix_quotes(autopool)
-                    print("sleeping 5 minutes")
-                    time.sleep(60 * 5)
+                    time.sleep(60 * 1)
                 except Exception as e:
                     print(f"Error fetching quotes for {autopool.name}, skipping. Error: {e}")
-                    time.sleep(60 * 5)
+                    time.sleep(60 * 1)
 
 
 if __name__ == "__main__":
