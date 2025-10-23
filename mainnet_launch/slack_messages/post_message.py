f11a9b3a140aac3eb35eff38849790d171b5b32a
parkerburchett <parkerburchett@gmail.com>
2025-10-23 09:57:08 -0700

    wip

--- mainnet_launch/slack_messages/post_message.py
+++ mainnet_launch/slack_messages/post_message.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 from pprint import pprint
-from blockkit import Message, Section, MarkdownText
 
 
 from mainnet_launch.constants import V2_DASHBOARD_NOTIFS_WEBHOOK_URL
@@ -9,31 +8,54 @@ from mainnet_launch.constants import V2_DASHBOARD_NOTIFS_WEBHOOK_URL
 from dotenv import load_dotenv
 import os
 
+
+
+
+from slack_sdk import WebClient
+
+
 load_dotenv()
 
+SLACK_OAUTH_TOKEN = os.environ.get("SLACK_OAUTH_TOKEN")
 TESTING_WEBHOOK_URL = os.environ.get("TESTING_SLACK_WEBHOOK_URL")
 
-
-def post_slack_message(message: Message):
-    payload = message.build()
-    resp = requests.post(TESTING_WEBHOOK_URL, json=payload, timeout=10)
-    resp.raise_for_status()
-    print(f"Message posted successfully, status code: {resp.status_code}")
+TESTING_CHANNEL_ID = 'C09MHUS35V0'
 
 
-def post_message_with_table(title: str, df: pd.DataFrame):
-    """
-    Post a slack message with a table.
 
-    Args:
-        title (str): The title of the message.
-        df (pd.DataFrame): The dataframe to post as a table.
-    """
-    table_contents = df.to_markdown(index=False)
+slack_client = WebClient(token=SLACK_OAUTH_TOKEN)
+auth_test = slack_client.auth_test()
+bot_user_id = auth_test["user_id"]
+print(f"App's bot user: {bot_user_id}")
 
-    markdown_table = "```\n" + table_contents + "```\n"
+files = client.files_list(user=bot_user_id)
 
-    message = Message(blocks=[Section(text=MarkdownText(text=title)), Section(text=MarkdownText(text=markdown_table))])
+# slack_client.files_u
+# # def post_slack_message_with_file(title:str, channel_id:str, df:pd.DataFrame):
 
-    post_slack_message(message)
-    pprint(df)
+    
+
+
+# def post_slack_message(message: Message):
+#     payload = message.build()
+#     resp = requests.post(TESTING_WEBHOOK_URL, json=payload, timeout=10)
+#     resp.raise_for_status()
+#     print(f"Message posted successfully, status code: {resp.status_code}")
+
+
+# def post_message_with_table(title: str, df: pd.DataFrame):
+#     """
+#     Post a slack message with a table.
+
+#     Args:
+#         title (str): The title of the message.
+#         df (pd.DataFrame): The dataframe to post as a table.
+#     """
+#     table_contents = df.to_markdown(index=False)
+
+#     markdown_table = "```\n" + table_contents + "```\n"
+
+#     message = Message(blocks=[Section(text=MarkdownText(text=title)), Section(text=MarkdownText(text=markdown_table))])
+
+#     post_slack_message(message)
+#     pprint(df)
--- tests/data_fetching/test_fetch_transactions.py
+++
@@ -1,1 +0,0 @@
-"""Make sure that we get transactions sent by an EOA address correctly from Etherscan, using pagination and > 10 addresses"""
