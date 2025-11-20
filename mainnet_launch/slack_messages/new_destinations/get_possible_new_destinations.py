6a98a674d0a8189d613ee8119ef81510a4a4c808
parkerburchett <parkerburchett@gmail.com>
2025-11-20 14:01:38 -0800

    wip

--- mainnet_launch/slack_messages/new_destinations/get_possible_new_destinations.py
+++ mainnet_launch/slack_messages/new_destinations/get_possible_new_destinations.py
@@ -1,15 +1,30 @@
-from mainnet_launch.database.postgres_operations import _exec_sql_and_cache
-from mainnet_launch.constants import *
-
-from multicall import Call
-import pandas as pd
-from mainnet_launch.data_fetching.get_state_by_block import get_state_by_one_block, identity_with_bool_success
-from web3 import Web3
-
 import requests
 import pandas as pd
 
+from web3 import Web3
+from multicall import Call
+
+from mainnet_launch.constants import (
+    ALL_AUTOPOOLS,
+    AutopoolConstants,
+    ETH_CHAIN,
+    BASE_CHAIN,
+    SONIC_CHAIN,
+    ARBITRUM_CHAIN,
+    PLASMA_CHAIN,
+    LINEA_CHAIN,
+    ALL_CHAINS,
+)
+from mainnet_launch.database.postgres_operations import _exec_sql_and_cache
+from mainnet_launch.data_fetching.get_state_by_block import get_state_by_one_block, identity_with_bool_success
+from mainnet_launch.slack_messages.post_message import SlackChannel, post_message_with_table
+
+
 DEFILLAMA_YIELDS_URL = "https://yields.llama.fi/pools"
+
+
+class SlackMessagesNewDestinationsError(Exception):
+    pass
 
 
 def fetch_all_defillama_yields() -> pd.DataFrame:
@@ -18,7 +33,6 @@ def fetch_all_defillama_yields() -> pd.DataFrame:
     payload = resp.json()
     df = pd.DataFrame.from_records(payload["data"])
     df["millions_usd"] = df["tvlUsd"] / 1_000_000
-    df["url"] = "https://www.defillama.com/yields/pool/" + df["pool"]
     return df
 
 
@@ -33,6 +47,7 @@ def fetch_an_autopools_valid_tokens(autopool: AutopoolConstants) -> list[str]:
         get_state_by_one_block([call], autopool.chain.get_block_near_top(), autopool.chain)["destinations"]
     )
     destinations = [Web3.toChecksumAddress(addr) for addr in destinations]
+
     get_destination_tokens_query = f"""
         SELECT DISTINCT
             dt.token_address,
@@ -53,7 +68,6 @@ def get_valid_rows_for_autopool(
     autopool: AutopoolConstants,
     df: pd.DataFrame,
     tvl_threshold: float = 1_000_000,
-    apy_threshold: float = 6.0,
 ) -> pd.DataFrame:
     autopool_tokens_lower = {str(t).lower() for t in fetch_an_autopools_valid_tokens(autopool)}
 
@@ -65,12 +79,11 @@ def get_valid_rows_for_autopool(
         n = len(tokens)
 
         project_str = (project or "").lower()
-        if "aura" in project_str:
-            # allow 1 token to be missing: require at least n-1 in the set
+        if "aura" in project_str or "balancer" in project_str:
             # accounts for composable stable pool tokens (lp token is in the underlying)
+            # note noisy, but ok for now
             return in_set_count >= n - 1
 
-        # default: require all tokens in the set
         return in_set_count == n
 
     underlying_tokens_in_autopool = df.apply(
@@ -86,6 +99,8 @@ def get_valid_rows_for_autopool(
         PLASMA_CHAIN: "Plasma",
         LINEA_CHAIN: "Linea",
     }
+    if len(ALL_CHAINS) != len(chain_to_defi_llama_yield_chain_name):
+        raise SlackMessagesNewDestinationsError("chain_to_defi_llama_yield_chain_name mapping is incomplete")
 
     right_chain = df["chain"] == chain_to_defi_llama_yield_chain_name[autopool.chain]
 
@@ -108,11 +123,7 @@ def get_valid_rows_for_autopool(
     valid_protocols_mask = df["project"].str.lower().isin(valid_protocols)
 
     valid_rows = df[
-        underlying_tokens_in_autopool
-        & right_chain
-        & valid_protocols_mask
-        & (df["tvlUsd"] >= tvl_threshold)
-        & (df["apyMean30d"] >= apy_threshold)
+        underlying_tokens_in_autopool & right_chain & valid_protocols_mask & (df["tvlUsd"] >= tvl_threshold)
     ].copy()
 
     valid_rows["autopool_name"] = autopool.name
@@ -120,20 +131,66 @@ def get_valid_rows_for_autopool(
 
 
 def fetch_possible_new_autopool_destinations(
-    tvl_threshold: float = 1_000_000, apy_threshold: float = 1.0
+    tvl_threshold: float = 1_000_000,
 ) -> pd.DataFrame:
     df = fetch_all_defillama_yields()
 
     valid_rows_list = []
     for autopool in ALL_AUTOPOOLS:
-        valid_rows = get_valid_rows_for_autopool(autopool, df, tvl_threshold, apy_threshold)
+        valid_rows = get_valid_rows_for_autopool(autopool, df, tvl_threshold)
         valid_rows_list.append(valid_rows)
 
     all_valid_rows_df = pd.concat(valid_rows_list, axis=0).reset_index(drop=True)
-    
-    return all_valid_rows_df, df
+
+    return all_valid_rows_df
+
+
+def extract_possible_interesting_destinations(df: pd.DataFrame) -> pd.DataFrame:
+    indexes = set()
+
+    for autopool in ALL_AUTOPOOLS:
+        sub_df = df[df["autopool_name"] == autopool.name]
+        if not sub_df.empty:
+            indexes.update([*sub_df["apyMean30d"].nlargest(2).index, *sub_df["apy"].nlargest(2).index])
+
+    interesting_df = df.loc[list(indexes)].reset_index(drop=True)
+    interesting_df = (
+        interesting_df.drop_duplicates(subset=["project", "symbol"])
+        .sort_values(by=["apy"], ascending=False)
+        .reset_index(drop=True)
+    )
+    interesting_df["copy_to_search"] = (
+        interesting_df["project"] + " " + interesting_df["symbol"] + " " + interesting_df["chain"]
+    )
+    interesting_columns = [
+        "copy_to_search",
+        "project",
+        "symbol",
+        "apyMean30d",
+        "apy",
+        "millions_usd",
+        "autopool_name",
+    ]
+
+    interesting_df["apyMean30d"] = interesting_df["apyMean30d"].map(lambda x: f"{x:.2f}%")
+    interesting_df["apy"] = interesting_df["apy"].map(lambda x: f"{x:.2f}%")
+    interesting_df["millions_usd"] = interesting_df["millions_usd"].map(lambda x: f"${x:.2f}M")
+
+    return interesting_df[interesting_columns]
+
+
+def post_slack_message_about_new_destinations():
+    df = fetch_possible_new_autopool_destinations(tvl_threshold=2_000_000)
+    interesting_df = extract_possible_interesting_destinations(df)
+
+    if not interesting_df.empty:
+        post_message_with_table(
+            channel=SlackChannel.TESTING,
+            initial_comment=f"{len(interesting_df)} Possible New Autopool Destinations (TVL >= $2M):",
+            df=interesting_df,
+            file_save_name="Possible Autopool Destinations.csv",
+        )
 
 
 if __name__ == "__main__":
-    df = fetch_possible_new_autopool_destinations()
-    df
+    post_slack_message_about_new_destinations()
--- mainnet_launch/slack_messages/send_informational_messages.py
+++ mainnet_launch/slack_messages/send_informational_messages.py
@@ -13,39 +13,15 @@ from mainnet_launch.slack_messages.depegs.asset_depegs import post_asset_depeg_s
 from mainnet_launch.slack_messages.post_message import post_slack_message
 
 
-def post_messages(slack_channel: SlackChannel):
-    post_slack_message(
-        slack_channel,
-        f"Informational messages run at {datetime.datetime.now().strftime('%Y-%m-%dT%H:%M')}",
-    )
-    visual_line_break = "-" * 40
-    post_slack_message(slack_channel, visual_line_break)
-    post_destination_ownership_exposure_table(slack_channel)
+def post_daily_messages(slack_channel: SlackChannel):
+    """Notifications that signal we should do *something*"""
     post_autopools_without_generated_plans(slack_channel)
     post_missing_balance_updated_events(slack_channel)
-    post_unsold_incentive_tokens(slack_channel)  # done
+    post_unsold_incentive_tokens(slack_channel)
+
+
+def post_weekly_messages(slack_channel: SlackChannel):
+    """Notifications that about the general state of the autopool, but don't require immediate action"""
+    post_destination_ownership_exposure_table(slack_channel)
     post_asset_depeg_slack_message(slack_channel)
     post_illiquid_token_holding_analysis(slack_channel)
-    post_slack_message(slack_channel, visual_line_break)
-    post_slack_message(slack_channel, visual_line_break)
-
-
-def send_production_slack_messages():
-    visual_line_break = "- - " * 30
-
-    channel = SlackChannel.PRODUCTION
-    post_slack_message(channel, visual_line_break)
-    post_slack_message(channel, visual_line_break)
-
-    post_autopools_without_generated_plans(channel)
-    post_missing_balance_updated_events(channel)
-    post_unsold_incentive_tokens(channel)
-
-
-def send_information_slack_messages():
-    # profile_function(post_messages, SlackChannel.TESTING)
-    send_production_slack_messages()
-
-
-if __name__ == "__main__":
-    send_information_slack_messages()
