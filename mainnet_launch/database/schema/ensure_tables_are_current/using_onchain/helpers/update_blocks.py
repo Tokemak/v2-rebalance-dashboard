e3e99b7f5d6359f27358bc929b62e57ccc1ec493
parkerburchett <parkerburchett@gmail.com>
2026-01-26 11:57:57 -0800

    wip

--- mainnet_launch/database/schema/ensure_tables_are_current/using_onchain/helpers/update_blocks.py
+++ mainnet_launch/database/schema/ensure_tables_are_current/using_onchain/helpers/update_blocks.py
@@ -13,16 +13,12 @@ from mainnet_launch.data_fetching.get_state_by_block import get_raw_state_by_blo
 from mainnet_launch.constants import *
 
 """
-We need the top and bottom block of each day. Exact top and exact bottom block are not necessary, as long as there is at least 23:59 minutes 
-between the top and bottom block timestamps for each day it si good enough
-
-This is because some blocks
-
+- We need the top and bottom block of each day
+- Exact top and exact bottom block are not necessary, as long as there is at least 23:59 minutes between the top and bottom block timestamps for each day it is good enough
+- This is because some chains have inconsistent block times
+- Some an have several (sequential blocks) with the same timestamp (sonic)
 """
 
-
-# note: this fetches some redundent blocks, but it is fine
-# can optimize later if needed fetching a few extra blocks for each day is reasonable
 
 DAY_COMPLETENESS_SECONDS = (24 * 60 * 60) - 60  # 23 hours 59 minutes
 
@@ -90,13 +86,15 @@ def _determine_missing_timestamps(chain: ChainData) -> list[int]:
 
     if missing_df.empty:
         return []
-    
+
     print(f"Chain {chain.name} missing days")
     print(missing_df.head(12))
     missing_ts = set()
 
     for unix_ts in missing_df["unix_timestamp"]:
-        N = 15
+        # note: this fetches some redundent blocks, but it is fine
+        # can optimize later if needed fetching a few extra blocks for each day is reasonable
+        N = 15  # I want to fetch blocks a bit before and after the target timestamps to be safe
         missing_ts.add(int(unix_ts) + N)
         missing_ts.add(int(unix_ts) - N)
         missing_ts.add(int(unix_ts + 86400 - N))
@@ -142,7 +140,7 @@ def ensure_blocks_is_current():
     for chain in ALL_CHAINS:
         unix_timestamps = _determine_missing_timestamps(chain)
         if len(unix_timestamps) == 0:
-            print(f"[{chain.name}] blocks table is already current.")
+            print(f"{chain.name} blocks table is already current.")
             continue
 
         blocks_to_add = fetch_blocks_by_unix_timestamps_defillama(
@@ -159,4 +157,5 @@ def ensure_blocks_is_current():
 
 if __name__ == "__main__":
     from mainnet_launch.constants import profile_function
+
     profile_function(ensure_blocks_is_current)
